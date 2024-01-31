71d057bf5357a0f8998a12e3b11ec5c70a2756cc
Nestor Castillo <nestorraulcgomez@gmail.com>
2024-01-30 22:20:41 -0300

    search-box component was added

---
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'shared-search-box',
+  templateUrl: './search-box.component.html',
+  styles: ``
+})
+export class SearchBoxComponent {
+
+  @Input() placeholder: string = '';
+  @Output() onValue = new EventEmitter();
+
+  emitValue(value : string) {
+    this.onValue.emit(value);
+  }
+
+}
